7098103e5c85e06758e92e19f249beecc8fb88ef
chaturvedi-mac-pro <harshitchaturvedi1996@gmail.com>
2025-02-08 13:00:25 +0530

    added leader highlighting functionality

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-// event handles
-
-// HOME
+// Selectors
 const homePlusOneBtn = document.querySelector(".home_plus_one");
 const homePlusTwoBtn = document.querySelector(".home_plus_two");
 const homePlusThreeBtn = document.querySelector(".home_plus_three");
@@ -12,38 +10,54 @@ const startNewGameBtn = document.querySelector(".new-game-btn");
 let home_score = 0;
 let guest_score = 0;
 
-homePlusOneBtn.addEventListener("click", () => {
-  home_score += 1;
-  console.log(home_score);
-  document.querySelector(".home_score").textContent = home_score;
-});
-homePlusTwoBtn.addEventListener("click", () => {
-  home_score += 2;
-  document.querySelector(".home_score").textContent = home_score;
-});
-homePlusThreeBtn.addEventListener("click", () => {
-  home_score += 3;
-  document.querySelector(".home_score").textContent = home_score;
-});
+// Event Listners
+homePlusOneBtn.addEventListener("click", () => incrementScore("home", 1));
+homePlusTwoBtn.addEventListener("click", () => incrementScore("home", 2));
+homePlusThreeBtn.addEventListener("click", () => incrementScore("home", 3));
 
-guestPlusOneBtn.addEventListener("click", () => {
-  guest_score += 1;
-  document.querySelector(".guest_score").textContent = guest_score;
-});
-guestPlusTwoBtn.addEventListener("click", () => {
-  guest_score += 2;
-  document.querySelector(".guest_score").textContent = guest_score;
-});
-guestPlusThreeBtn.addEventListener("click", () => {
-  guest_score += 3;
-  document.querySelector(".guest_score").textContent = guest_score;
-});
+guestPlusOneBtn.addEventListener("click", () => incrementScore("guest", 1));
+guestPlusTwoBtn.addEventListener("click", () => incrementScore("guest", 2));
+guestPlusThreeBtn.addEventListener("click", () => incrementScore("guest", 3));
+startNewGameBtn.addEventListener("click", startNewGame);
 
-startNewGameBtn.addEventListener("click", start_new_game);
+// Functions
+function incrementScore(team, points) {
+  if (team === "home") {
+    home_score += points;
+    displayScore(team);
+    checkAndHiglightLeader();
+  } else if (team === "guest") {
+    guest_score += points;
+    displayScore(team);
+    checkAndHiglightLeader();
+  }
+}
 
-function start_new_game() {
+function displayScore(team) {
+  if (team === "home") {
+    document.querySelector(".home_score").textContent = home_score;
+  } else if (team === "guest") {
+    document.querySelector(".guest_score").textContent = guest_score;
+  }
+}
+
+function startNewGame() {
   home_score = 0;
   guest_score = 0;
   document.querySelector(".home_score").textContent = home_score;
   document.querySelector(".guest_score").textContent = guest_score;
+  document.querySelector(".home-score-card").style.backgroundColor = "black";
+  document.querySelector(".guest-score-card").style.backgroundColor = "black";
+}
+
+function checkAndHiglightLeader() {
+  document.querySelector(".home-score-card").style.backgroundColor = "black";
+  document.querySelector(".guest-score-card").style.backgroundColor = "black";
+
+  if (home_score > guest_score) {
+    document.querySelector(".home-score-card").style.backgroundColor = "green";
+  }
+  if (guest_score > home_score) {
+    document.querySelector(".guest-score-card").style.backgroundColor = "green";
+  }
 }
